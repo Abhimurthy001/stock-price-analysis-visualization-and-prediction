7c74a76a833fded8b6039109e4765ad4498aa605
Abhi murthy <90611136+Abhimurthy001@users.noreply.github.com>
2023-02-09 00:09:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-START = "2015-01-01"
+START = "2002-01-23"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+stocks = ('HDFCBANK.NS', 'RELIANCE.NS', 'ICICIBANK.NS', 'TCS.NS','INFY.NS','SBIN.NS','ADANIPOWER.NS','ADANIPORTS.NS')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-n_years = st.slider('Years of prediction:', 1, 4)
+n_years = st.slider('Years of prediction:', 1, 5)
 period = n_years * 365
 
 
