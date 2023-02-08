c5427eed4ace37d0b669a0c66a0b1aba998037da
Abhi murthy <90611136+Abhimurthy001@users.noreply.github.com>
2023-02-08 11:28:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,118 +1,65 @@
-# load library
+# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
-import numpy as np
-import pandas as pd
-from pandas_datareader import data as pdr
+from datetime import date
 
-import math
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
-from matplotlib import style
-import datetime as dt
 import yfinance as yf
-import datetime
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
 
-# print title of web app
-st.title("Stock price analysis and prediction")
-st.markdown("> Its a project on analysing, visualizing and predicting stock price.")
-st.markdown("> It is a web application build on streamlit")
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
-# Create a text element and let the reader know the data is loading.
+st.title('Stock Forecast App')
+
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+	
 data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
 
-# Load data from yahoo finance.
-start=dt.date(2010,1,1)
-end=dt.date.today()
-data=pdr.get_data_yahoo("SBIN.NS", start, end)
+st.subheader('Raw data')
+st.write(data.tail())
 
-#fill nan vale with next value within columns
-data.fillna(method="ffill",inplace=True)
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
 
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-# create checkbox
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
- 
-# show the description of data
-st.subheader('Detail description about Datasets:-')
-descrb=data.describe()
-st.write(descrb)
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
 
-#create new columns like year, month, day
-data["Year"]=data.index.year
-data["Month"]=data.index.month
-data["Weekday"]=data.index.day_name()
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
 
-# dislay graph of open and close column
-st.subheader('Graph of Close & Open:-')
-st.line_chart(data[["Open","Close"]])
-
-# display plot of Adj Close column in datasets
-st.subheader('Graph of Adjacent Close:-')
-st.line_chart(data['Adj Close'])
-
-# display plot of volume column in datasets
-st.subheader('Graph of Volume:-')
-st.line_chart(data['Volume'])
-
-# create new cloumn for data analysis.
-data['HL_PCT'] = (data['High'] - data['Low']) / data['Close'] * 100.0
-data['PCT_change'] = (data['Close'] - data['Open']) / data['Open'] * 100.0
-data = data[['Adj Close', 'HL_PCT', 'PCT_change', 'Volume']]
-
-# display the new dataset after modificaton
-st.subheader('Newly format DataSet:-')
-st.dataframe(data.tail(500))
-
-forecast_col = 'Adj Close'
-forecast_out = int(math.ceil(0.01 * len(data)))
-data['label'] = data[forecast_col].shift(-forecast_out)
-
-X = np.array(data.drop(['label'], 1))
-X = preprocessing.scale(X)
-X_lately = X[-forecast_out:]
-X = X[:-forecast_out]
-data.dropna(inplace=True)
-y = np.array(data['label'])
-
-# split dataset into train and test dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-clf = LinearRegression(n_jobs=-1)
-clf.fit(X_train, y_train)
-confidence = clf.score(X_test, y_test)
-
-# display the accuracy of forecast value.
-st.subheader('Accuracy:')
-st.write(confidence)
-
-forecast_set = clf.predict(X_lately)
-data['Forecast'] = np.nan
-
-last_date = data.iloc[-1].name
-last_unix = last_date.timestamp()
-one_day = 86400
-next_unix = last_unix + one_day
-
-for i in forecast_set:
-    next_date = datetime.datetime.fromtimestamp(next_unix)
-    next_unix += 86400
-    data.loc[next_date] = [np.nan for _ in range(len(data.columns)-1)]+[i]
-    last_date = data.iloc[-1].name
-    dti = pd.date_range(last_date, periods=forecast_out+1, freq='D')
-    index = 1
-for i in forecast_set:
-    data.loc[dti[index]] = [np.nan for _ in range(len(data.columns)-1)] + [i]
-    index +=1
-
-# display the forecast value.
-st.subheader('Forecast value :-')
-st.dataframe(data.tail(50))
-
-# display the graph of adj close and forecast columns
-st.subheader('Graph of Adj Close and Forecast :-')
-st.line_chart(data[["Adj Close","Forecast"]])
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
