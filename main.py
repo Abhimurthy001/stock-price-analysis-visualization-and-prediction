3ffe62124751f1338f50ee3d8ccd65f2db148f54
Abhi murthy <90611136+Abhimurthy001@users.noreply.github.com>
2023-02-08 14:02:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,42 @@
-# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
-from datetime import date
-
-import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
-from plotly import graph_objs as go
-
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
-
-st.title('Stock Forecast App')
-
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
-
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
+import yfinance as finance
 
 
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
+def get_ticker(name):
+	company = finance.Ticker(name) # google
+	return company
 
-	
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
 
-st.subheader('Raw data')
-st.write(data.tail())
+# Project Details
+st.title("Build and Deploy Stock Market App Using Streamlit")
+st.header("A Basic Data Science Web Application")
+st.sidebar.header("Geeksforgeeks \n TrueGeeks")
 
-# Plot raw data
-def plot_raw_data():
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-	st.plotly_chart(fig)
-	
-plot_raw_data()
+company1 = get_ticker("GOOGL")
+company2 = get_ticker("MSFT")
 
-# Predict forecast with Prophet.
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+# fetches the data: Open, Close, High, Low and Volume
+google = finance.download("GOOGL", start="2021-10-01", end="2021-10-01")
+microsoft = finance.download("MSFT", start="2021-10-01", end="2021-10-01")
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+# Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+data1 = company1.history(period="3mo")
+data2 = company2.history(period="3mo")
 
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
-    
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
+# markdown syntax
+st.write("""
+### Google
+""")
 
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+# detailed summary on Google
+st.write(company1.info['longBusinessSummary'])
+st.write(google)
+
+# plots the graph
+st.line_chart(data1.values)
+
+st.write("""
+### Microsoft
+""")
+st.write(company2.info['longBusinessSummary'], "\n", microsoft)
+st.line_chart(data2.values)
