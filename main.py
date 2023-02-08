b50d03e571c68b0403354a8a1975bdb5f0616309
Abhi murthy <90611136+Abhimurthy001@users.noreply.github.com>
2023-02-08 14:20:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ st.title("Build and Deploy Stock Market App Using Streamlit")
 st.header("A Basic Data Science Web Application")
 st.sidebar.header("Geeksforgeeks \n TrueGeeks")
 
-company1 = get_ticker("GOOGL")
-company2 = get_ticker("MSFT")
+company1 = get_ticker("SBIN.NS")
+company2 = get_ticker("ADANIPOWER.NS")
 
 # fetches the data: Open, Close, High, Low and Volume
-google = finance.download("GOOGL", start="2021-10-01", end="2021-10-01")
-microsoft = finance.download("MSFT", start="2021-10-01", end="2021-10-01")
+google = finance.download("SBIN.NS", start="2021-10-01", end="2021-10-01")
+microsoft = finance.download("ADANIPOWER.NS", start="2021-10-01", end="2021-10-01")
 
 # Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
 data1 = company1.history(period="3mo")
@@ -30,7 +30,7 @@ st.write("""
 
 # detailed summary on Google
 st.write(company1.info['longBusinessSummary'])
-st.write(google)
+st.write(sbi)
 
 # plots the graph
 st.line_chart(data1.values)
@@ -38,5 +38,5 @@ st.line_chart(data1.values)
 st.write("""
 ### Microsoft
 """)
-st.write(company2.info['longBusinessSummary'], "\n", microsoft)
+st.write(company2.info['longBusinessSummary'], "\n", adanipower)
 st.line_chart(data2.values)
